1d5cf3873b3a138250db0abd9bdd2a47c4684b42
Eric Lombrozo <elombrozo@gmail.com>
2013-01-07 08:07:51 -0800

    Removed net.cpp's dependency on init.h.
    Added explicit include of main.h in init.cpp, changed include of init.h to include of main.h in net.cpp.
    
    Added function registration for net.cpp in init.cpp's network initialization.
    
    Removed protocol.cpp's dependency on main.h.
    
    TODO: Remove main.h include in net.cpp.
    
    Conflicts:
    	src/init.cpp
    
    Conflicts:
    	src/net.cpp
    
    Former-commit-id: d8dfd371d52bddd2c32fd73340af2403d3e13255

--- src/net.cpp
+++ src/net.cpp
@@ -5,8 +5,8 @@
 
 #include "db.h"
 #include "net.h"
-#include "init.h"
 #include "strlcpy.h"
+#include "main.h"
 #include "addrman.h"
 #include "ui_interface.h"
 
@@ -69,6 +69,28 @@ vector<std::string> vAddedNodes;
 CCriticalSection cs_vAddedNodes;
 
 static CSemaphore *semOutbound = NULL;
+
+//
+// Handlers that need to be registered
+//
+static ProcessMessagesHandler fnProcessMessages = NULL;
+static SendMessagesHandler fnSendMessages = NULL;
+static StartShutdownHandler fnStartShutdown = NULL;
+
+void SetProcessMessagesHandler(ProcessMessagesHandler handler)
+{
+    fnProcessMessages = handler;
+}
+
+void SetSendMessagesHandler(SendMessagesHandler handler)
+{
+    fnSendMessages = handler;
+}
+
+void SetStartShutdownHandler(StartShutdownHandler handler)
+{
+    fnStartShutdown = handler;
+}
 
 void AddOneShot(string strDest)
 {
@@ -1571,8 +1593,8 @@ void ThreadMessageHandler()
             // Send messages
             {
                 TRY_LOCK(pnode->cs_vSend, lockSend);
-                if (lockSend)
-                    SendMessages(pnode, pnode == pnodeTrickle);
+                if (lockSend && fnSendMessages)
+                    fnSendMessages(pnode, pnode == pnodeTrickle);
             }
             boost::this_thread::interruption_point();
         }
