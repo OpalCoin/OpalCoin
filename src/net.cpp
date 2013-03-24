b366a13f1b566253e42e5c3a66503b7ebeaf0b9c
Matt Corallo <git@bluematt.me>
2013-03-24 19:38:19 -0400

    (finally) Remove IRC Seed support now that lfnet is down.
    Conflicts:
    	blackcoin-qt.pro
    	doc/coding.txt
    	src/init.cpp
    	src/irc.cpp
    
    Conflicts:
    	src/init.cpp
    	src/irc.cpp
    	src/makefile.bsd
    
    Former-commit-id: 8433d45e7731ef3be9a165c1adcc222467f2f004

--- src/net.cpp
+++ src/net.cpp
@@ -3,7 +3,6 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "irc.h"
 #include "db.h"
 #include "net.h"
 #include "init.h"
@@ -337,7 +336,6 @@ bool GetMyExternalIP2(const CService& addrConnect, const char* pszGet, const cha
     return error("GetMyExternalIP() : connection closed");
 }
 
-// We now get our external IP from the IRC server first and only use this as a backup
 bool GetMyExternalIP(CNetAddr& ipRet)
 {
     CService addrConnect;
@@ -1779,10 +1777,6 @@ void StartNode(boost::thread_group& threadGroup)
     // Map ports with UPnP
     MapPort(GetBoolArg("-upnp", USE_UPNP));
 #endif
-
-    // Get addresses from IRC and advertise ours
-    if (!NewThread(ThreadIRCSeed, NULL))
-        printf("Error: NewThread(ThreadIRCSeed) failed\n");
 
     // Send and receive from sockets, accept connections
     threadGroup.create_thread(boost::bind(&TraceThread<void (*)()>, "net", &ThreadSocketHandler));
