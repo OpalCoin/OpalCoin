3b172f3bf635f187a92c3f66b3d75e4367e62015
Wladimir J. van der Laan <laanwj@gmail.com>
2014-04-15 12:43:17 +0200

    Add AssertLockHeld for cs_main to ChainActive-using functions
    All functions that use ChainActive but do not aquire the cs_main
    lock themselves, need to be called with the cs_main lock held.
    
    This commit adds assertions to all externally callable functions
    that use chainActive or chainMostWork.
    
    This will flag usages when built with -DDEBUG_LOCKORDER.
    
    Conflicts:
    	src/main.cpp
    	src/qt/transactiondesc.cpp
    	src/qt/transactionrecord.cpp
    
    Conflicts:
    	src/main.cpp
    
    Former-commit-id: 13c74ed0c98efcf0c505cb6156dd86dd5202561c

--- src/main.h
+++ src/main.h
@@ -472,6 +472,7 @@ public:
 
     bool IsFinal(int nBlockHeight=0, int64_t nBlockTime=0) const
     {
+        AssertLockHeld(cs_main);
         // Time based nLockTime implemented in 0.1.6
         if (nLockTime == 0)
             return true;
