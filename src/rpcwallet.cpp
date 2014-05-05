5c1a734814bb6200800f7daa3e181d0b6ffa7ff7
Kamil Domanski <kdomanski@kdemail.net>
2014-05-05 20:08:13 +0200

    switch from boost int types to <stdint.h>
    Conflicts:
    	src/rpcblockchain.cpp
    	src/rpcclient.cpp
    	src/rpcmining.cpp
    	src/rpcmisc.cpp
    	src/rpcnet.cpp
    	src/rpcrawtransaction.cpp
    	src/rpcwallet.cpp
    
    Conflicts:
    	src/bitcoinrpc.cpp
    	src/rpcnet.cpp
    	src/rpcwallet.cpp
    
    Former-commit-id: 8a68bf12345c8172103ba39fb787c5370f3e9699

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -61,7 +61,6 @@ void WalletTxToJSON(const CWalletTx& wtx, Object& entry)
     entry.push_back(Pair("txid", wtx.GetHash().GetHex()));
     entry.push_back(Pair("time", (int64_t)wtx.GetTxTime()));
     entry.push_back(Pair("timereceived", (int64_t)wtx.nTimeReceived));
-    
     BOOST_FOREACH(const PAIRTYPE(string,string)& item, wtx.mapValue)
         entry.push_back(Pair(item.first, item.second));
 }
