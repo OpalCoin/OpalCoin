bc9bf83645ef8c7d2623433afafcb5d344e317a8
Wladimir J. van der Laan <laanwj@gmail.com>
2014-06-10 15:01:23 +0200

    Remove unused Print/PrintHex functions
    You can just use HexStr(script) or script.ToString() for debugging, no
    need for these extra functions.
    
    Conflicts:
    	src/script.h
    	src/util.h
    
    Former-commit-id: cfdf9d3abf2e3fa78f2dabe19218b109a7b33922

--- src/script.h
+++ src/script.h
@@ -550,12 +550,6 @@ public:
     void SetDestination(const CTxDestination& address);
     void SetMultisig(int nRequired, const std::vector<CKey>& keys);
 
-
-    void PrintHex() const
-    {
-        printf("CScript(%s)\n", HexStr(begin(), end(), true).c_str());
-    }
-
     std::string ToString(bool fShort=false) const
     {
         std::string str;
@@ -577,11 +571,6 @@ public:
                 str += GetOpName(opcode);
         }
         return str;
-    }
-
-    void print() const
-    {
-        printf("%s\n", ToString().c_str());
     }
 
     CScriptID GetID() const
