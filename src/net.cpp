fe6c6c1e8dc900212b06fde0bfbec365d9ee20cf
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-06-22 14:51:38 +0200

    remove unused CNode::Cleanup()
    Conflicts:
    	src/net.cpp
    
    Former-commit-id: a99e6f7f5aefc7d5374e6362869cf1169f2c6805

--- src/net.cpp
+++ src/net.cpp
@@ -541,11 +541,6 @@ void CNode::CloseSocketDisconnect()
     }
 }
 
-void CNode::Cleanup()
-{
-}
-
-
 void CNode::PushVersion()
 {
     /// when NTP implemented, change to just nTime = GetAdjustedTime()
@@ -851,7 +846,6 @@ void ThreadSocketHandler2(void* parg)
 
                     // close socket and cleanup
                     pnode->CloseSocketDisconnect();
-                    pnode->Cleanup();
 
                     // hold in disconnected pool until all refs are released
                     if (pnode->fNetworkNode || pnode->fInbound)
