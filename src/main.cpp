ee790ce52000cbf40c4ddff887bdfdf8a47a800c
Jeff Garzik <jgarzik@bitpay.com>
2014-07-29 10:53:38 -0400

    ui_interface: remove unused NotifyBlocksChanged signal
    Conflicts:
    	src/main.cpp
    	src/qt/clientmodel.cpp
    
    Former-commit-id: 805563bb81a3c897f11c4b80a50e56f4add1c890

--- src/main.cpp
+++ src/main.cpp
@@ -1130,7 +1130,6 @@ void static InvalidChainFound(CBlockIndex* pindexNew)
     {
         nBestInvalidTrust = pindexNew->nChainTrust;
         CTxDB().WriteBestInvalidTrust(CBigNum(nBestInvalidTrust));
-        uiInterface.NotifyBlocksChanged();
     }
 
     uint256 nBestInvalidBlockTrust = pindexNew->nChainTrust - pindexNew->pprev->nChainTrust;
@@ -1975,7 +1974,6 @@ bool CBlock::AddToBlockIndex(unsigned int nFile, unsigned int nBlockPos, const u
         hashPrevBestCoinBase = vtx[0].GetHash();
     }
 
-    uiInterface.NotifyBlocksChanged();
     return true;
 }
 
