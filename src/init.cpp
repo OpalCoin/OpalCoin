50c779c285f9049a72378ff2a78c96a48fe22ca0
Whit J <whitj00@users.noreply.github.com>
2014-09-07 18:22:33 -0700

    Update init.cpp
    Former-commit-id: 5424b3d1a33a9a8de37c66fa7d7f78f5c4e76516

--- src/init.cpp
+++ src/init.cpp
@@ -10,6 +10,7 @@
 #include "util.h"
 #include "ui_interface.h"
 #include "checkpoints.h"
+#include "smessage.h"
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <boost/filesystem/convenience.hpp>
