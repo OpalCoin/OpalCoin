51e9de7144d0d17e67274ee2b34c694237c0c77e
Whit J <whitj00@users.noreply.github.com>
2014-09-26 09:23:58 -0700

    Update messagemodel.h
    Former-commit-id: 8cde184d456bbd79d610cfe83cdd1b0f084cdf29 [formerly 5481b3ee8a543e64c0716bf4f73b35f8621c49a7]
    Former-commit-id: 8cde184d456bbd79d610cfe83cdd1b0f084cdf29
    Former-commit-id: d3cd3debd4aefd8bec3e018eccee809d02bc476a
    Former-commit-id: cdd70e2df7408bb828bdcd1e133ed52264d3a1af

--- src/qt/messagemodel.h
+++ src/qt/messagemodel.h
@@ -67,7 +67,7 @@ struct MessageTableEntry
     }
 };
 
-/** Interface to ShadowCoin Secure Messaging from Qt view code. */
+/** Interface to Opalcoin Secure Messaging from Qt view code. */
 class MessageModel : public QAbstractTableModel
 {
     Q_OBJECT
